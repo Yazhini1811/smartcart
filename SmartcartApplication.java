5626766f065b5a7705a372847a0aed70ba16e2bc
Yazhini P <yazhinipalaniappan2004@gmail.com>
2026-04-14 13:36:12 +0530

    Initialize SmartcartApplication with Spring Boot

---
+++ SmartcartApplication.java
@@ -0,0 +1,12 @@
+package com.yazhini.smartcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcartApplication.class, args);
+    }
+}
